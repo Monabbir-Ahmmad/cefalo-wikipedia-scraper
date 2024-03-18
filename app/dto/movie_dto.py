cfc4247a4f5732b33df556fb2cb0ee2160d9ccd3
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2024-03-18 16:04:07 +0600

    Add movie repository and movie service interfaces

--- app/dto/movie_dto.py
+++ app/dto/movie_dto.py
@@ -2,11 +2,6 @@ def response(movie) -> dict:
     return {
         **movie,
         "_id": str(movie["_id"]),
-        "title": movie["title"],
-        "link": movie["link"],
-        "year": movie["year"],
-        "awards": movie["awards"],
-        "nominations": movie["nominations"]
     }
 
 def list_response(movies) -> list:
