1434969cf71aee7f4d427c1f2d34794a289fd7b0
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2024-03-25 13:41:54 +0600

    Fix: router name fix

--- app/api/v1/route.py
+++ app/api/v1/route.py
@@ -3,7 +3,4 @@ from app.api.v1.endpoints import movie
 
 router = APIRouter()
 
-router_list = [movie.router]
-
-for router in router_list:
-    router.include_router(router)
+router.include_router(movie.router)
